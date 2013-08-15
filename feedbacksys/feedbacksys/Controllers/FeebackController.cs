d51fc75c933dfdb1cc3e814a70d2191496154c76
Brevin <Brevin.Li@activenetwork.com>
2013-08-15 15:20:26 +0800

    commit changes

---
+++ feedbacksys/feedbacksys/Controllers/FeebackController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Http;
+using feedbacksys.Domain;
+using feedbacksys.Domain.Repository;
+
+namespace feedbacksys.Controllers
+{
+    public class FeebackController : ApiController
+    {
+        [System.Web.Mvc.HttpPost]
+        public void Post(Image img)
+        {
+           
+            ControllerContext.Request.Headers.Add("Access-Control-Allow-Origin", "*");
+            ControllerContext.Request.Headers.Add("Access-Control-Allow-Credentials", "true");
+            var repository = new ImageRepository();
+            repository.Create(img);
+        }
+    }
+}
