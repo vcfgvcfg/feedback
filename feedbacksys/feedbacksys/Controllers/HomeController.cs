9a59ec8f9a977655f3599aa3d9d964016f1fd70c
Brevin <Brevin.Li@activenetwork.com>
2013-08-15 15:21:09 +0800

    d

--- feedbacksys/feedbacksys/Controllers/HomeController.cs
+++ feedbacksys/feedbacksys/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using feedbacksys.Domain;
 using feedbacksys.Domain.Repository;
 
 namespace feedbacksys.Controllers
